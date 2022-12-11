8171e5d4a043b3930a204141b72a623fb62fd4d9
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-12-11 04:55:27 -0500

    Initial Commit

---
+++ Debate/DebateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebateApp.swift
+//  Debate
+//
+//  Created by Tim Yoon on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DebateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
